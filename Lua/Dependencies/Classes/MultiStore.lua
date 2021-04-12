974ad8fde5c2e21a5fee1d17f42541bea291c70c
Haashim <45342729+Inctus@users.noreply.github.com>
2021-04-12 17:55:36 +0100

    Create MultiStore.lua

---
+++ Lua/Dependencies/Classes/MultiStore.lua
@@ -0,0 +1,31 @@
+--<< SERVICES >>
+local DataStoreService = game:GetService("DataStoreService")
+local HttpService = game:GetService("HttpService")
+
+--<< MODULE >>
+local MultiStore = {}
+MultiStore.__index = MultiStore
+
+--<< FUNCTIONS >>
+function MultiStore.new(name, scope)
+	local self = setmetatable({}, MultiStore)
+	
+	self.MainDataStore = DataStoreService:GetDataStore(name, scope)
+	
+	return self
+end
+
+function MultiStore:PullData(save_key)
+	return MultiStore.Utility.Safe.GetAsync(self.MainDataStore, save_key)
+end
+
+function MultiStore:PushData(save_key, data)
+	return MultiStore.Utility.Safe.SetAsync(self.MainDataStore, save_key, data)
+end
+
+function MultiStore:UpdateData(save_key, update_function)
+	return MultiStore.Utility.Safe.UpdateAsync(self.MainDataStore, save_key, update_function)
+end
+
+--<< RETURNEE >>
+return MultiStore
