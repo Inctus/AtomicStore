94d7884f1cfb09e558e34f8012c20a673a256d75
Haashim <45342729+Inctus@users.noreply.github.com>
2021-04-13 12:09:40 +0100

    Renamed Modules>Shared
    Fixed bug with InstantiatedStores, and  fixed naming.
    
    Co-Authored-By: Hasnain Raza <49315454+Hasnain123Raza@users.noreply.github.com>

--- Lua/AtomicStore.lua
+++ Lua/AtomicStore.lua
@@ -1,36 +1,36 @@
 --<< DEPENDENCIES >>
 local Dependencies = script.Parent:WaitForChild("Dependencies")
 local Classes = Dependencies:WaitForChild("Classes")
-local Modules = Dependencies:WaitForChild("Modules")
+local Shared = Dependencies:WaitForChild("Shared")
 
 --<< MODULE >>
 local AtomicStore = {}
-AtomicStore._InstanciatedStores = {}
+AtomicStore._InstantiatedStores = {}
 
 --<< FUNCTIONS >>
 local function Initialise()
 	local _classes = {}
-	local _modules = {}
+	local _shared = {}
 	
 	for _, class in pairs(Classes:GetChildren()) do
 		_classes[class.Name] = require(class)
 	end
 	
-	for _, module in pairs(Modules:GetChildren()) do
-		local _required = require(module)
-		_modules[module.Name] = _required
+	for _, shared_module in pairs(Shared:GetChildren()) do
+		local _required = require(shared_module)
+		_shared[shared_module.Name] = _required
 		for _, class in pairs(_classes) do
-			class[module.Name] = _required
+			class[shared_module.Name] = _required
 		end
 	end
 	
 	Classes = _classes
-	Modules = _modules
+	Shared = _shared
 end
 
 local function InternalRetrieve(store_type, ...)
 	local new_store = Classes[store_type].new(...)
-	table.insert(AtomicStore._InstanciatedStores, new_store)
+	AtomicStore._InstantiatedStores[new_store.Name] = new_store
 	return new_store
 end
 
@@ -54,14 +54,14 @@ end
 --<< STORE MANAGEMENT >>
 function AtomicStore:GetStores()
 	local _list = {}
-	for name, store in pairs(AtomicStore._InstanciatedStores) do
+	for name, store in pairs(AtomicStore._InstantiatedStores) do
 		table.insert(_list, store)
 	end
 	return _list
 end
 
 function AtomicStore:FindFirstStoreByName(name)
-	return AtomicStore._InstanciatedStores[name]
+	return AtomicStore._InstantiatedStores[name]
 end
 
 --<< INITILISATION >>
