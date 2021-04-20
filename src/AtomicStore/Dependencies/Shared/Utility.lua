b57b8421292a12d56b3917fd050c19e9c361efda
Haashim <45342729+Inctus@users.noreply.github.com>
2021-04-20 09:32:07 +0100

    Utility abstracted
    Added retryrequest method to abstract repeated boilerplate code.

--- src/AtomicStore/Dependencies/Shared/Utility.lua
+++ src/AtomicStore/Dependencies/Shared/Utility.lua
@@ -27,101 +27,85 @@ function Utility.Ternary(condition, a, b)
 	end
 end
 
+function Utility.RetryRequest(datastore_request_type, retry_function, returns)
+	local data, retries, success = nil, 0, false
+
+	repeat
+		retries = retries + 1
+		while not DataStoreService:GetRequestBudgetForRequestType(datastore_request_type) do
+			Utility.Wait(RETRY_TIME)
+		end
+		success, data = pcall(retry_function)
+	until success or (retries > MAX_RETRIES)
+
+	return Utility.Ternary(returns, Utility.Ternary(success, data, success), success) 
+end
+
 --<< SAFE >>
 Utility.Safe = {}
 
 function Utility.Safe.GetAsync(datastore, save_key)
-	local data, retries, success = nil, 0, false
-
-	repeat 
-		retries = retries + 1
-		while not DataStoreService:GetRequestBudgetForRequestType(Enum.DataStoreRequestType.GetAsync) do
-			Utility.Wait(RETRY_TIME)
-		end
-		success = pcall(function()
-			data = datastore:GetAsync(
+	return Utility.RetryRequest(
+		Enum.DataStoreRequestType.GetAsync,
+		function()
+			return datastore:GetAsync(
 				save_key
 			)
-		end)
-	until success or (retries>MAX_RETRIES)
+		end,
+		true
+	)
+end
 
-	return Utility.Ternary(success, data, success)
+function Utility.Safe.GetOrderedAsync(ordered_datastore, page_size)
+	return Utility.RetryRequest(
+		Enum.DataStoreRequestType.GetSortedAsync,
+		function()
+			return ordered_datastore:GetSortedAsync(
+				false,
+				page_size
+			)
+		end,
+		true
+	)
+end
+
+function Utility.Safe.UpdateAsync(datastore, save_key, update_function)
+	return Utility.RetryRequest(
+		Enum.DataStoreRequestType.UpdateAsync,
+		function()
+			return datastore:UpdateAsync(
+				save_key,
+				update_function
+			)
+		end,
+		true
+	)
 end
 
 function Utility.Safe.SetAsync(datastore, save_key, data)
-	local retries, success = 0, false
-
-	repeat
-		retries = retries + 1
-		while not DataStoreService:GetRequestBudgetForRequestType(Enum.DataStoreRequestType.SetIncrementAsync) do
-			Utility.Wait(RETRY_TIME)
-		end
-		success = pcall(function()
+	return Utility.RetryRequest(
+		Enum.DataStoreRequestType.SetIncrementAsync,
+		function()
 			datastore:SetAsync(
 				save_key,
 				data
 			)
-		end)
-	until success or (retries>MAX_RETRIES)
-	
-	return success
-end
-
-function Utility.Safe.UpdateAsync(datastore, save_key, update_function)
-	local data, retries, success = nil, 0, false
-
-	repeat
-		retries = retries + 1
-		while not DataStoreService:GetRequestBudgetForRequestType(Enum.DataStoreRequestType.UpdateAsync) do
-			Utility.Wait(RETRY_TIME)
-		end
-		success, data = pcall(function()
-			datastore:UpdateAsync(
-				save_key,
-				update_function
-			)
-		end)
-	until success or (retries>MAX_RETRIES)
-
-	return Utility.Ternary(success, data, success)
-end
-
-function Utility.Safe.GetOrderedAsync(ordered_datastore, page_size)
-	local book, retries, success = nil, 0, false
-
-	repeat 
-		retries = retries + 1
-		while not DataStoreService:GetRequestBudgetForRequestType(Enum.DataStoreRequestType.GetSortedAsync) > 0 do
-			Utility.Wait(RETRY_TIME)
-		end
-		success = pcall(function()
-			book = ordered_datastore:GetSortedAsync(
-				false,
-				page_size
-			)
-		end)
-	until success or (retries>MAX_RETRIES) 
-
-	return Utility.Ternary(success, book, success)
+		end,
+		false
+	)
 end
 
 function Utility.Safe.SetOrderedAsync(ordered_datastore, save_key, data)
-	local retries, success = 0, false
-
-	repeat 
-		retries = retries + 1
-		while not DataStoreService:GetRequestBudgetForRequestType(Enum.DataStoreRequestType.SetIncrementSortedAsync) > 0 do
-			Utility.Wait(RETRY_TIME)
-		end
-		success = pcall(function()
+	return Utility.RetryRequest(
+		Enum.DataStoreRequestType.SetIncrementSortedAsync,
+		function()
 			ordered_datastore:SetAsync(
 				save_key,
 				data
 			)
-		end)
-	until success or (retries > MAX_RETRIES)
-
-	return success
+		end,
+		false
+	)
 end
 
 --<< RETURNEE >>
